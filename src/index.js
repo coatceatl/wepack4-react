ed0d19cf8d339c00bc53cf41d77b5b561068b13a
Oksana Tkachenko <oksana@crystax.net>
2018-08-11 11:51:08 +0300

    added react component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('webpack 4 + react');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <h1>Hello from React</h1>
+}
+
+ReactDOM.render(
+  <App />, document.getElementById('root')
+);
